bb44ca7fda0dab11a77be892cc226ea815642165
Ryan-Calmon <ryancalmon05@gmail.com>
2025-06-11 02:06:51 -0300

    adicionando footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/footer.css";
+
+function Footer(){
+    return(
+        <div>
+           <footer className="footer">
+            <p>&copy; BrothersCup 2025. Todos os direitos reservados.</p>
+            </footer>
+
+        </div>
+    );
+
+}
+export default Footer;
